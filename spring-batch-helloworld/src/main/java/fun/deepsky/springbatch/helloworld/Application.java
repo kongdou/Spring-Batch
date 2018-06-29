f63a00d6645510ec568bca9c400bdd97dd360ee6
kongdou <zhao.xiao.jie@outlook.com>
2018-06-29 08:03:09 +0800

    spring batch helloworld
    spring batch helloworld

---
+++ spring-batch-helloworld/src/main/java/fun/deepsky/springbatch/helloworld/Application.java
@@ -0,0 +1,25 @@
+package fun.deepsky.springbatch.helloworld;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("job.xml");
+		JobLauncher launcher =  (JobLauncher) context.getBean("jobLauncher");
+		Job job = (Job) context.getBean("billJob");
+		try {
+			JobExecution result = launcher.run(job, new JobParameters());
+			System.out.println(result.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	
+	}
+}
---
+++ spring-batch-helloworld/src/main/java/fun/deepsky/springbatch/helloworld/CreditBillProcessor.java
@@ -0,0 +1,17 @@
+package fun.deepsky.springbatch.helloworld;
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class CreditBillProcessor implements ItemProcessor<CreditBill, CreditBill>{
+
+	@Override
+	public CreditBill process(CreditBill bill) throws Exception {
+		System.out.println(bill.getName());
+		// TODO Auto-generated method stub
+		return bill;
+	}
+
+
+
+	 
+}
