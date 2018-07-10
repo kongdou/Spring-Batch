c804c5c7795fb82fa2ca9181bd533947dcb8d016
kongdou <zhao.xiao.jie@outlook.com>
2018-07-10 12:40:55 +0800

    spring batch 写多文件
    spring batch 写多文件

---
+++ spring-batch-writer/src/main/java/fun/deepsky/springbatch/writer/xml/MultiFileWriterApplication.java
@@ -0,0 +1,24 @@
+package fun.deepsky.springbatch.writer.xml;
+
+import java.util.Date;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MultiFileWriterApplication {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("job-writer-multifile.xml");
+		JobLauncher launcher = (JobLauncher) context.getBean("jobLauncher");
+		Job job = (Job) context.getBean("mulitFileWriterJob");
+		try {
+			launcher.run(job, new JobParametersBuilder().addDate("date", new Date()).toJobParameters());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
