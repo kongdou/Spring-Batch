8d99f1ab24f3814626492f0be71903820c99a1dc
kongdou <zhao.xiao.jie@outlook.com>
2018-07-17 18:18:16 +0800

    spring batch step end
    spring batch step end

---
+++ spring-batch-flow/src/main/java/fun/deepsky/springbatch/flow/ConditionalEndApplication.java
@@ -0,0 +1,30 @@
+package fun.deepsky.springbatch.flow;
+
+import java.util.Date;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ConditionalEndApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("job-conditional-end.xml");
+
+		JobLauncher launcher = (JobLauncher) context.getBean("jobLauncher");
+
+		Job job = (Job) context.getBean("conditionalEndJob");
+
+		try {
+			launcher.run(job, new JobParametersBuilder().addDate("date", new Date())
+					.addString("inputFile","classpath:/data/credit-card-bill-201310-notexist.zip")
+					.addString("readFileName","credit-card-bill-201310.csv")
+					.addString("workDirectory", "file:target/work/")
+					.addString("writeTarget", "file:target/outputFile_end.csv").toJobParameters());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
