337c851e8bc65148564f742462878f69b6c0b3ce
kongdou <zhao.xiao.jie@outlook.com>
2018-07-17 17:04:43 +0800

    spring batch flowstep
    spring batch flowstep

---
+++ spring-batch-flow/src/main/java/fun/deepsky/springbatch/flow/FlowApplication.java
@@ -0,0 +1,30 @@
+package fun.deepsky.springbatch.flow;
+
+import java.util.Date;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class FlowApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("job-flow-step.xml");
+
+		JobLauncher launcher = (JobLauncher) context.getBean("jobLauncher");
+
+		Job job = (Job) context.getBean("externalFlowJob");
+
+		try {
+			launcher.run(job, new JobParametersBuilder().addDate("date", new Date())
+					.addString("inputFile","classpath:/data/credit-card-bill-201310.zip")
+					.addString("readFileName","credit-card-bill-201310.csv")
+					.addString("workDirectory", "file:target/work/")
+					.addString("writeTarget", "file:target/split/outputFile_10.csv").toJobParameters());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
